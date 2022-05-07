e82f00b8eb03f6f87e9a9aeb65b7aa3c8fb89178
ramitjansIA <99759058+ramitjansIA@users.noreply.github.com>
2022-05-07 13:49:51 +0200

    Update 02_Preprocessing.py
    Se finaliza una primera versión del preprocessing de los datos.

--- 02_Preprocessing.py
+++ 02_Preprocessing.py
@@ -11,6 +11,8 @@ Created on Sat May  7 11:57:13 2022
 import pandas as pd
 import re
 import string
+from tabulate import tabulate
+
 
 # =============================================================================
 # PARAMETROS:
@@ -25,71 +27,115 @@ hoteles = pd.read_pickle(DATASETS_DIR + 'HotelesIBIZA.pkl')
 # =============================================================================
 # Realizamos el PREPROCESSING:
 ### Hotel 
-
-hotelPR
-
+hotelPR = pd.DataFrame(hoteles.Hotel)
 
 # -----------------------------------------------------------------------------
 ### Estrellas
+estrellas = [re.sub(" estrellas| estrella", "", estr) for estr in hoteles['Estrellas']]
+hotelPR['Estrellas'] = [float(estr) if estr != '' else float("nan") for estr in estrellas ]
+
 # -----------------------------------------------------------------------------
 ### Ratio 
+ratio = [re.sub("\n.", "", rat) for rat in hoteles['Ratio']]
+hotelPR['Ratio'] = [float(re.sub(",", "", rat)) if rat != '' else float("nan") for rat in ratio]
+
 # -----------------------------------------------------------------------------
 ### Ratio_descr 
+print(tabulate(pd.crosstab(index = hoteles['Ratio_descr'], columns = "count"), 
+               headers = 'firstrow', tablefmt = 'fancy_grid'))
+hotelPR['ratioDescr'] = hoteles['Ratio_descr']
+
 # -----------------------------------------------------------------------------
 ### Direcciones
+#### No lo vamos a utilizar
+
 # -----------------------------------------------------------------------------
 ### Ammenities 
+ammenities = [am.split("\n") for am in hoteles['Ammenities']]
+df = pd.get_dummies(pd.DataFrame(ammenities))
+df.columns = df.columns.str.split("_").str[-1]
+df = df.groupby(df.columns.map(string.capwords), axis=1).sum()
+hotelPR = pd.concat([hotelPR, df], axis = 1)
+
 # -----------------------------------------------------------------------------
 ### Servicios_Principales 
+# servPrincipales = [am.split("\n") for am in hoteles['Servicios_Principales']]
+# df = pd.get_dummies(pd.DataFrame(servPrincipales))
+# df.columns = df.columns.str.split("_").str[-1]
+# df = df.groupby(df.columns.map(string.capwords), axis=1).sum()
+# hotelPR = pd.concat([hotelPR, df], axis = 1)
+
+### SR: Aquí se debería de hacer mediante match buscando aquella información que queramos escoger
+
 # -----------------------------------------------------------------------------
 ### CaractFamilias
+#### No lo vamos a utilizar
+
 # -----------------------------------------------------------------------------
 ### lugaresInteres 
+lugaresInteres = [am.split("\n") for am in hoteles['lugaresInteres']]
+lugaresInteres = [am.split(":")[0] for ams in lugaresInteres for am in ams]
+lugaresInteres = list(set(lugaresInteres))
+
+### SR: En caso de que no sepas lugares de interés, aquí teemos una lista de 182: lugaresInteres
+
 # -----------------------------------------------------------------------------
 ### Transporte 
+#### No lo vamos a utilizar
+
 # -----------------------------------------------------------------------------
 ### tamanyo 
+#### plantas
+plantas = [re.findall(r'[0-9]+ planta', x) for x in hoteles['tamanyo']]
+plantas = [re.sub(" planta|\[|\]|\'", "", str(p)) for p in plantas]
+hotelPR['Plantas'] = [int(plt) if plt != '' else float("nan") for plt in plantas]
 
-import re
-plantas = [re.findall(r'[0-9]+ planta', x) for x in hoteles.tamanyo]
-plantas = [re.sub(" planta", "", str(p)) for p in plantas]
+habitaciones = [re.findall(r'[0-9]+ habita', x) for x in  hoteles['tamanyo']]
+habitaciones = [re.sub(" habita|\[|\]|\'", "", str(h)) for h in habitaciones]
+hotelPR['habitaciones'] = [int(hab) if hab != '' else float("nan") for hab in habitaciones]
 
-plantas_final =  []
-for p in plantas: 
-    try: 
-        plantas_final.append(int(p[2]))
-    except:
-        plantas_final.append(0)
+hotelPR['ratioHabPlanta'] = hotelPR['habitaciones']/hotelPR['Plantas']
         
-habitaciones = [re.findall(r'[0-9]+ habita', x) for x in hoteles.tamanyo]
-habitaciones = [re.sub(" habita", "", str(h)) for h in habitaciones]
-
-habitaciones_final =  []
-for p in habitaciones: 
-    try: 
-        habitaciones_final.append(int(p[2]))
-    except:
-        habitaciones_final.append(0)
-        
-        
-
-
 # -----------------------------------------------------------------------------
 ### mascotas 
+#### SR: Por ahora no se como realizarla
+
 # -----------------------------------------------------------------------------
 ### internet
+#### SR: Por ahora no se como realizarla
+
 # -----------------------------------------------------------------------------
 ### aparcamiento 
+#### SR: Por ahora no se como realizarla
+
 # -----------------------------------------------------------------------------
 ### masHab 
+#### SR: Por ahora no se como realizarla
+
 # -----------------------------------------------------------------------------
 ### ocioInstalaciones 
+#### SR: Por ahora no se como realizarla
+
 # -----------------------------------------------------------------------------
 ### ocioCercanias
+#### SR: Por ahora no se como realizarla
+
 # -----------------------------------------------------------------------------
 ### habitaciones 
+#### SR: Por ahora no se como realizarla
+
 # -----------------------------------------------------------------------------
 ### coordenadas 
+hotelPR['latitud'] = [float(hot[0].split(",")[0]) for hot in hoteles['coordenadas']]
+hotelPR['longitud'] = [float(hot[0].split(",")[1]) for hot in hoteles['coordenadas']]
+
 # -----------------------------------------------------------------------------
 ### precio
+hotelPR['precio'] = [float(re.sub(" €", "", pr)) if pr != '' else float("nan") for pr in hoteles['precio']]
+
+# =============================================================================
+# Guardamos en formato de pickle
+hotelPR.to_pickle(DATASETS_DIR + "HotelesPreprocesados.pkl")
+hotelPR.to_csv(DATASETS_DIR + "HotelesPreprocesados.csv")
+
 # =============================================================================
