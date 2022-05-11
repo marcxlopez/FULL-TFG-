fe91648023d745cacbfd6ca338c212400256f7ee
ramitjansIA <99759058+ramitjansIA@users.noreply.github.com>
2022-05-11 22:51:49 +0200

    Update 05_ClusterJerarquico.py
    Script con sólo la implementación del cluster. Además se añade el bucle para calcular un rango de clasificación. Se acaba graficando el estadístico de sillhouete para saber quien es el mejor corte.

--- 05_ClusterJerarquico.py
+++ 05_ClusterJerarquico.py
@@ -2,116 +2,97 @@
 """
 Created on Wed May 11 16:50:50 2022
 
-@author: marcl
+@author: Marc Lopez
 """
 
+# Cargamos las librerias necesarias
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 %matplotlib inline
 
+## Librerias para los clusters
+import scipy.cluster.hierarchy as shc
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.metrics import silhouette_score
+
+
+# =============================================================================
+# Parámetros del modelo: 
+PATH = "E:\\DOCENCIA\\TFG Alumnos\\MARC LOPEZ\\REPOSITORIO\\"
+DATASETS_DIR = PATH + "data\\"
+
+#-----------------------------------------------------------------------
 #Load the data and look at the first few rows
 PATH = "C:\\Users\marcl\\Desktop\\TFG\\GITHUB TFG\\"
 DATASETS_DIR = PATH + "data\\"
 
-data = pd.read_csv(DATASETS_DIR + 'HotelesModelos.csv',sep=';')
-#-----------------------------------------------------------------------
-###N1. NORMALIZAR DATOS 
-#crear matriz numerica 
-datan = data.drop(['Hotel','ratioDescr'],axis=1) 
-datanm = datan[['Plantas','habitaciones','ratioHabPlanta']]
+# =============================================================================
+# Cargamos la base de datos
+hotelesNorm = pd.read_pickle(DATASETS_DIR + 'HotelesNormalizados.pkl')
+hoteles = pd.read_pickle(DATASETS_DIR + 'HotelesImputados.pkl')
 
-##FILL NAN WITH MICE 
-
-import numpy as np
-from sklearn.experimental import enable_iterative_imputer
-from sklearn.impute import IterativeImputer
-from sklearn.linear_model import LinearRegression
-
-lr = LinearRegression()
-imp = IterativeImputer(estimator=lr,missing_values=np.nan, max_iter=30, verbose=2, imputation_order='roman',random_state=0)
-X=imp.fit_transform(datanm)
-X[:,0] = np.around(X[:,0], decimals=0)
-X[:,1] = np.round(X[:,1],decimals=0)
-# introducir X en datan como 'Plantas', 'habitaciones', 'ratioHabPlanta'
-datan['Plantas'] = X[:,0]
-datan['habitaciones'] = X[:,1]
-datan['ratioHabPlanta'] = X[:,2]
-
-#quitar los datos omitidos (RATIO)
-#quitar nan en datan
-datan = datan.dropna()
-
-
-##NORMALIZAR DATOS 
-from sklearn.preprocessing import normalize
-data_scaled = normalize(datan)
-data_scaled = pd.DataFrame(data_scaled, columns=datan.columns)
-data_scaled.head()
-
-corr = data_scaled.corr()
-
+# =============================================================================
 #Let’s first draw the dendrogram to help us 
 #decide the number of clusters for this particular problem
 
-import scipy.cluster.hierarchy as shc
 plt.figure(figsize=(10, 7))  
 plt.title("Dendrograms")  
-dend = shc.dendrogram(shc.linkage(data_scaled, method='ward'))
+dend = shc.dendrogram(shc.linkage(hotelesNorm, method='ward'))
 
+silluete = []
+kden = range(2, 7)
 
+for k in kden:
+    print("Realizamos agrupación de k = " + str(k))
+    
+    # Calculamos la clasificación con el número k 
+    cluster = AgglomerativeClustering(n_clusters = k, affinity = 'euclidean', 
+                                      linkage = 'ward')  
+    # Predecimos el número de clases
+    cluster.fit_predict(hotelesNorm)
 
-###Ahora queremos separar dos grupos 
-#así conseguiremos dos clusters
-plt.figure(figsize=(10, 7))  
-plt.title("Dendrograms")  
-dend = shc.dendrogram(shc.linkage(data_scaled, method='ward'))
-plt.axhline(y=6, color='r', linestyle='--')
+    # Gráficamos el corte del dendograma
+    plt.figure(figsize=(10, 7))  
+    plt.title("Dendograma para k = " + str(k))  
+    dend = shc.dendrogram(shc.linkage(hotelesNorm, method = 'ward', 
+                                      metric = 'euclidean'), labels = cluster.labels_)
+    plt.axhline(y = 6, color = 'r', linestyle = '--')
 
+    # Graficamos dicho corte en las variables precio vs. distnacia en la variable original
+    # plt.figure(figsize = (10, 7))  
+    # plt.scatter(hotelesNorm['precio'], hotelesNorm['distancia'], 
+    #            c = cluster.labels_) 
+    # plt.xlabel('precios')
+    # plt.ylabel('distancia')
+    # plt.title('Clustering Jerárquico con k =' + str(k))
+    # plt.legend(range(1, k + 1))
 
+    # Calculamos el estadístico de sillhouete para ver cual es la mejor agrupación
+    silluete.append(silhouette_score(hotelesNorm, cluster.labels_, metric = 'euclidean', 
+                     random_state = 0))
+    
+# Graficamos el estadistico de la sillhouete
+plt.plot(kden, silluete, '--bo', label = 'Sillhouette')
 
-#cluster jerárquico para dos grupos
+# Seleccionamos el mejor
+kOptima = kden[np.argmax(silluete)]
 
-from sklearn.cluster import AgglomerativeClustering
-cluster = AgglomerativeClustering(n_clusters=2, affinity='euclidean', linkage='ward')  
-cluster.fit_predict(data_scaled)
+# Calculamos la clasificación con el número k 
+cluster = AgglomerativeClustering(n_clusters = kOptima, affinity = 'euclidean', 
+                                  linkage = 'ward')  
+# Predecimos el número de clases
+cluster.fit_predict(hotelesNorm)
 
-plt.figure(figsize=(10, 7))  
-plt.scatter(data_scaled['precios'], data_scaled['distancia'], c=cluster.labels_) 
-
-##cluster para tres grupos 
-
-cluster = AgglomerativeClustering(n_clusters=3, affinity='euclidean', linkage='ward')  
-cluster.fit_predict(data_scaled)
-
-plt.figure(figsize=(10, 7))  
-plt.scatter(data_scaled['precios'], data_scaled['distancia'], c=cluster.labels_) 
+# Graficamos los valores de la mejor clasificación
+plt.figure(figsize = (10, 7))  
+plt.scatter(hotelesNorm['precio'], hotelesNorm['distancia'], 
+            c = cluster.labels_) 
 plt.xlabel('precios')
 plt.ylabel('distancia')
-plt.title('Hierarchical Clustering')
-#cluster para cuatro grupos 
+plt.title('Clustering Jerárquico con k =' + str(kOptima))
+plt.legend(range(1, kOptima + 1))
 
-cluster = AgglomerativeClustering(n_clusters=4, affinity='euclidean', linkage='ward')  
-cluster.fit_predict(data_scaled)
-
-plt.figure(figsize=(10, 7))  
-plt.scatter(data_scaled['precios'], data_scaled['distancia'], c=cluster.labels_) 
-plt.xlabel('precios')
-plt.ylabel('distancia')
-plt.title('Hierarchical Clustering')
-#cluster para cinco grupos 
-
-cluster = AgglomerativeClustering(n_clusters=5, affinity='euclidean', linkage='ward')  
-cluster.fit_predict(data_scaled)
-
-plt.figure(figsize=(10, 7))  
-plt.scatter(data_scaled['precios'], data_scaled['distancia'], c=cluster.labels_) 
-#'precios' for x axis, 'distancia' for y axis
-plt.xlabel('precios')
-plt.ylabel('distancia')
-plt.title('Hierarchical Clustering')
-
-
-
+# =============================================================================
 
         
