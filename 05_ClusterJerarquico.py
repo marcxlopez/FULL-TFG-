cbe9417a9ca82999126e3f34ee55dcbb7a80ab13
ramitjansIA <99759058+ramitjansIA@users.noreply.github.com>
2022-05-14 11:20:04 +0200

    Update 05_ClusterJerarquico.py
    Se añade una partición a partir de un árbol de decisión

--- 05_ClusterJerarquico.py
+++ 05_ClusterJerarquico.py
@@ -17,6 +17,8 @@ from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics import silhouette_score,accuracy_score
 from sklearn.cluster import KMeans
 
+from sklearn.tree import _tree, DecisionTreeClassifier
+
 # =============================================================================
 # Parámetros del modelo: 
 #PATH = "E:\\DOCENCIA\\TFG Alumnos\\MARC LOPEZ\\REPOSITORIO\\"
@@ -73,6 +75,7 @@ for k in kden:
                      random_state = 0))
     #printeamos el estadístico de sillhouete
     print("El estadístico de sillhouete para k = " + str(k) + " es: " + str(silluete[-1]))
+
 # Graficamos el estadistico de la sillhouete
 plt.plot(kden, silluete, '--bo', label = 'Sillhouette')
 
@@ -88,18 +91,17 @@ cluster.fit_predict(hotelesNorm)
 
 # Graficamos los valores de la mejor clasificación
 plt.figure(figsize = (10, 7))  
-plt.scatter(hotelesNorm['precios'], hotelesNorm['distancia'], 
+plt.scatter(hotelesNorm['precio'], hotelesNorm['distancia'], 
             c = cluster.labels_) 
-plt.xlabel('precios')
+plt.xlabel('precio')
 plt.ylabel('distancia')
 plt.title('Clustering Jerárquico con k =' + str(kOptima))
-plt.legend(range(1, kOptima + 1))
+plt.legend(range(0, kOptima))
 
 # =============================================================================
-###Adjusted Rand Index
-        
+###Adjusted Rand Index 
 labels_pred = cluster.labels_
-labels_true = hoteles['precios']
+labels_true = hoteles['precio']
 from sklearn.metrics.cluster import adjusted_rand_score
 adjusted_rand_score(labels_true, labels_pred)
 
@@ -124,18 +126,16 @@ kmeans = KMeans(n_clusters = 2, init = 'k-means++', max_iter = 300,
                 n_init = 10, random_state = 0)
 kmeans.fit(hotelesNorm)
 labels_pred = kmeans.labels_
-labels_true = hoteles['precios']
+labels_true = hoteles['precio']
 
 # Graficamos los valores de la mejor clasificación
 plt.figure(figsize = (10, 7))
-plt.scatter(hotelesNorm['precios'], hotelesNorm['distancia'],
+plt.scatter(hotelesNorm['precio'], hotelesNorm['distancia'],
             c = kmeans.labels_) 
 plt.xlabel('precios')
 plt.ylabel('distancia')
 plt.title('Clustering K-Means con k = 2')
 plt.legend(range(1, kOptima + 1))
-
-
 
 ###PROFILING-------------------------------------------------------------------
 df_features = hotelesNorm
@@ -163,3 +163,69 @@ df_profile = df_cluster_summary.join(df_profile_overall) # joins on Index
 #cluster 0 == precios bajos, mucha distancia al mar 
 #cluster 1 == precios medios altos, mas cerca del mar, mas habitaciones
 
+
+
+# =================================================================================
+# Definimos unas funciones que nos permitan describir nuestros datos
+
+def get_class_rules(tree: DecisionTreeClassifier, feature_names: list):
+  inner_tree: _tree.Tree = tree.tree_
+  classes = tree.classes_
+  class_rules_dict = dict()
+
+  def tree_dfs(node_id=0, current_rule=[]):
+    # feature[i] holds the feature to split on, for the internal node i.
+    split_feature = inner_tree.feature[node_id]
+    if split_feature != _tree.TREE_UNDEFINED: # internal node
+      name = feature_names[split_feature]
+      threshold = inner_tree.threshold[node_id]
+      # left child
+      left_rule = current_rule + ["({} <= {})".format(name, threshold)]
+      tree_dfs(inner_tree.children_left[node_id], left_rule)
+      # right child
+      right_rule = current_rule + ["({} > {})".format(name, threshold)]
+      tree_dfs(inner_tree.children_right[node_id], right_rule)
+    else: # leaf
+      dist = inner_tree.value[node_id][0]
+      dist = dist/dist.sum()
+      max_idx = dist.argmax()
+      if len(current_rule) == 0:
+        rule_string = "ALL"
+      else:
+        rule_string = " and ".join(current_rule)
+      # register new rule to dictionary
+      selected_class = classes[max_idx]
+      class_probability = dist[max_idx]
+      class_rules = class_rules_dict.get(selected_class, [])
+      class_rules.append((rule_string, class_probability))
+      class_rules_dict[selected_class] = class_rules
+    
+  tree_dfs() # start from root, node_id = 0
+  return class_rules_dict
+
+def cluster_report(data: pd.DataFrame, clusters, min_samples_leaf=50, pruning_level=0.01):
+    # Create Model
+    tree = DecisionTreeClassifier(min_samples_leaf=min_samples_leaf, ccp_alpha=pruning_level)
+    tree.fit(data, clusters)
+    
+    # Generate Report
+    feature_names = data.columns
+    class_rule_dict = get_class_rules(tree, feature_names)
+
+    report_class_list = []
+    for class_name in class_rule_dict.keys():
+        rule_list = class_rule_dict[class_name]
+        combined_string = ""
+        for rule in rule_list:
+            combined_string += "[{}] {}\n\n".format(rule[1], rule[0])
+        report_class_list.append((class_name, combined_string))
+        
+    cluster_instance_df = pd.Series(clusters).value_counts().reset_index()
+    cluster_instance_df.columns = ['class_name', 'instance_count']
+    report_df = pd.DataFrame(report_class_list, columns=['class_name', 'rule_list'])
+    report_df = pd.merge(cluster_instance_df, report_df, on='class_name', how='left')
+    return(report_df.sort_values(by='class_name')[['class_name', 'instance_count', 'rule_list']])
+
+
+report = cluster_report(hotelesNorm, cluster.labels_)
+print(report)
