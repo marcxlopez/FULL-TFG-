d6d8ea5eb01d4f58c0a81126c4f56a7249bd3b90
marcxlopez <86571675+marcxlopez@users.noreply.github.com>
2022-05-18 17:58:23 +0200

    Reorganización del 05_CLUSTER

--- 05_01_ClusterJerarquico.py
+++ 05_01_ClusterJerarquico.py
@@ -80,8 +80,8 @@ for k in kden:
 plt.plot(kden, silluete, '--bo', label = 'Sillhouette')
 
 # Seleccionamos el mejor
-kOptima = kden[np.argmax(silluete)] #me coge 6
-#kOptima = 2 #est sillhouete o.431
+#kOptima = kden[np.argmax(silluete)] #me coge 6
+kOptima = 2 #est sillhouete o.431
 
 # Calculamos la clasificación con el número k 
 cluster = AgglomerativeClustering(n_clusters = kOptima, affinity = 'euclidean', 
@@ -91,9 +91,9 @@ cluster.fit_predict(hotelesNorm)
 
 # Graficamos los valores de la mejor clasificación
 plt.figure(figsize = (10, 7))  
-plt.scatter(hotelesNorm['precio'], hotelesNorm['distancia'], 
+plt.scatter(hotelesNorm['precios'], hotelesNorm['distancia'], 
             c = cluster.labels_) 
-plt.xlabel('precio')
+plt.xlabel('precios')
 plt.ylabel('distancia')
 plt.title('Clustering Jerárquico con k =' + str(kOptima))
 plt.legend(range(0, kOptima))
@@ -101,131 +101,7 @@ plt.legend(range(0, kOptima))
 # =============================================================================
 ###Adjusted Rand Index 
 labels_pred = cluster.labels_
-labels_true = hoteles['precio']
+labels_true = hoteles['precios']
 from sklearn.metrics.cluster import adjusted_rand_score
 adjusted_rand_score(labels_true, labels_pred)
-
-#------------------------------------------------------------------------------
-
-################################################################################
-###Bucle para realizar diferentes clusterings KMEANS 
-#comparar COEFICIENTE DE SILHOUETTE
-
-for k in range(2,8):
-    #realizar el clustering
-    kmeans = KMeans(n_clusters=k)
-    kmeans.fit(hotelesNorm)
-    #calcular el silhouette score
-    silhouette_avg = silhouette_score(hotelesNorm, kmeans.labels_)
-    print("For n_clusters =", k, "The average silhouette_score is :", silhouette_avg)
-
-#------------------------------------------------------------------------------
-##Realizamos KMEANS con 2 clusters 
-#porque el COEFICIENTE DE SILHOUETTE es demasiado similar entre grupos 
-kmeans = KMeans(n_clusters = 2, init = 'k-means++', max_iter = 300,
-                n_init = 10, random_state = 0)
-kmeans.fit(hotelesNorm)
-labels_pred = kmeans.labels_
-labels_true = hoteles['precio']
-
-# Graficamos los valores de la mejor clasificación
-plt.figure(figsize = (10, 7))
-plt.scatter(hotelesNorm['precio'], hotelesNorm['distancia'],
-            c = kmeans.labels_) 
-plt.xlabel('precios')
-plt.ylabel('distancia')
-plt.title('Clustering K-Means con k = 2')
-plt.legend(range(1, kOptima + 1))
-
-###PROFILING-------------------------------------------------------------------
-df_features = hotelesNorm
-# Add cluster labels
-df_features['cluster_ids'] = kmeans.labels_
-
-# Overall level summary
-df_profile_overall = df_features.describe().T
-
-# using mean; use appropriate summarization (median, count, etc.) for each feature
-df_profile_overall['Overall Dataset'] = df_profile_overall[['mean']]
-df_profile_overall = df_profile_overall[['Overall Dataset']]
-
-# Cluster ID level summary
-df_cluster_summary = df_features.groupby('cluster_ids').describe().T.reset_index()
-df_cluster_summary = df_cluster_summary.rename(columns={'level_0':'column','level_1':'metric'})
-
-# using mean; use appropriate summarization (median, count, etc.) for each feature
-df_cluster_summary = df_cluster_summary[df_cluster_summary['metric'] == "mean"]
-df_cluster_summary = df_cluster_summary.set_index('column')
-
-# join into single summary dataset
-df_profile = df_cluster_summary.join(df_profile_overall) # joins on Index
-
-#cluster 0 == precios bajos, mucha distancia al mar 
-#cluster 1 == precios medios altos, mas cerca del mar, mas habitaciones
-
-
-
-# =================================================================================
-# Definimos unas funciones que nos permitan describir nuestros datos
-
-def get_class_rules(tree: DecisionTreeClassifier, feature_names: list):
-  inner_tree: _tree.Tree = tree.tree_
-  classes = tree.classes_
-  class_rules_dict = dict()
-
-  def tree_dfs(node_id=0, current_rule=[]):
-    # feature[i] holds the feature to split on, for the internal node i.
-    split_feature = inner_tree.feature[node_id]
-    if split_feature != _tree.TREE_UNDEFINED: # internal node
-      name = feature_names[split_feature]
-      threshold = inner_tree.threshold[node_id]
-      # left child
-      left_rule = current_rule + ["({} <= {})".format(name, threshold)]
-      tree_dfs(inner_tree.children_left[node_id], left_rule)
-      # right child
-      right_rule = current_rule + ["({} > {})".format(name, threshold)]
-      tree_dfs(inner_tree.children_right[node_id], right_rule)
-    else: # leaf
-      dist = inner_tree.value[node_id][0]
-      dist = dist/dist.sum()
-      max_idx = dist.argmax()
-      if len(current_rule) == 0:
-        rule_string = "ALL"
-      else:
-        rule_string = " and ".join(current_rule)
-      # register new rule to dictionary
-      selected_class = classes[max_idx]
-      class_probability = dist[max_idx]
-      class_rules = class_rules_dict.get(selected_class, [])
-      class_rules.append((rule_string, class_probability))
-      class_rules_dict[selected_class] = class_rules
-    
-  tree_dfs() # start from root, node_id = 0
-  return class_rules_dict
-
-def cluster_report(data: pd.DataFrame, clusters, min_samples_leaf=50, pruning_level=0.01):
-    # Create Model
-    tree = DecisionTreeClassifier(min_samples_leaf=min_samples_leaf, ccp_alpha=pruning_level)
-    tree.fit(data, clusters)
-    
-    # Generate Report
-    feature_names = data.columns
-    class_rule_dict = get_class_rules(tree, feature_names)
-
-    report_class_list = []
-    for class_name in class_rule_dict.keys():
-        rule_list = class_rule_dict[class_name]
-        combined_string = ""
-        for rule in rule_list:
-            combined_string += "[{}] {}\n\n".format(rule[1], rule[0])
-        report_class_list.append((class_name, combined_string))
-        
-    cluster_instance_df = pd.Series(clusters).value_counts().reset_index()
-    cluster_instance_df.columns = ['class_name', 'instance_count']
-    report_df = pd.DataFrame(report_class_list, columns=['class_name', 'rule_list'])
-    report_df = pd.merge(cluster_instance_df, report_df, on='class_name', how='left')
-    return(report_df.sort_values(by='class_name')[['class_name', 'instance_count', 'rule_list']])
-
-
-report = cluster_report(hotelesNorm, cluster.labels_)
-print(report)
+#El índice 0 nos indica que los dos clusters son significativamente diferentes. 
