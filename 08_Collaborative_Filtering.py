853fe66223c5dabba73059a00ae0b8e4399b877c
marcxlopez <86571675+marcxlopez@users.noreply.github.com>
2022-05-24 19:05:36 +0200

    Update 08_Collaborative_Filtering.py
    intentando reenfocar el recomender

--- 08_Collaborative_Filtering.py
+++ 08_Collaborative_Filtering.py
@@ -10,6 +10,9 @@ from surprise.model_selection import GridSearchCV
 import pandas as pd
 from surprise import Dataset
 from surprise import Reader
+from scipy import spatial
+import pandas as pd 
+
 #Load the data and look at the first few rows
 PATH = "C:\\Users\marcl\\Desktop\\TFG\\GITHUB TFG\\"
 DATASETS_DIR = PATH + "data\\"
@@ -18,38 +21,22 @@ DATASETS_DIR = PATH + "data\\"
 # Cargamos la base de datos
 hotelesNorm = pd.read_pickle(DATASETS_DIR + 'HotelesNormalizados.pkl')
 hoteles = pd.read_pickle(DATASETS_DIR + 'HotelesImputados.pkl')
+i = 0
+distancia_R = []
+dist_R = 100
+#crear una funcion que recoja la variable distancia 
+def recomender(dist_R):
+    i = 0
+    while i<701:
+        #append spatial.distance.euclidean(hoteles['distancia'][i], dist_R) in hoteles['Euclidean_d']
+        try: 
+            distancia_R.append(spatial.distance.euclidean(hoteles['distancia'][i], dist_R))
+            i = i + 1
+        except KeyError:
+            i = i + 1
+    Recomender = pd.DataFrame()            
+    Recomender['Distancia_R'] = distancia_R
+    Recomender['Nombre'] = hoteles['Hotel']
+    #ordenar Recomender['Distancia_R'] de menor a mayor 
+    Recomender = Recomender.sort_values(by=['Distancia_R'])    
 
-trainingSet = Dataset.load_from_df(hoteles, reader=Reader(name=None, line_format=u'user item rating', sep=None, rating_scale=(1, 5), skip_lines=0))
-
-#==============================================================================
-# recommender.py
-# To use item-based cosine similarity
-sim_options = {
-    "name": "cosine", #metrica de similutd a utilizar 
-    "user_based": False,  #False = basado en el item
-}
-algo = KNNWithMeans(sim_options=sim_options)
-trainingSet = Dataset.load_from_df(hoteles, reader=Reader(name=None, line_format=u'user item rating', sep=None, rating_scale=(1, 5), skip_lines=0))
-#no va
-algo.fit(trainingSet)
-prediction = algo.predict(hoteles['Hotel'],hoteles['distancia'])#pero no se que poner 
-prediction.est
-
-#------------------------------------------------------------------------------
-###Tuning the Algorithm Parameters
-
-##For SVD (Model based)
-
-#data = Dataset.load_builtin("ml-100k")
-
-param_grid = {
-    "n_epochs": [5, 10],
-    "lr_all": [0.002, 0.005],
-    "reg_all": [0.4, 0.6]
-}
-gs = GridSearchCV(SVD, param_grid, measures=["rmse", "mae"], cv=3)
-
-gs.fit(hoteles)
-
-print(gs.best_score["rmse"])
-print(gs.best_params["rmse"])
