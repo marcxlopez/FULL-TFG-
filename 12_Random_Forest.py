d695b441d53fd48baeeac593cb0c5aaa07c54eb8
marcxlopez <86571675+marcxlopez@users.noreply.github.com>
2022-06-07 18:16:58 +0200

    Create 12_Random_Forest.py

---
+++ 12_Random_Forest.py
@@ -0,0 +1,392 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  7 17:54:59 2022
+
+@author: marcl
+"""
+
+# Tratamiento de datos
+# ==============================================================================
+import numpy as np
+import pandas as pd
+
+# Gráficos
+# ==============================================================================
+import matplotlib.pyplot as plt
+
+# Preprocesado y modelado
+# ==============================================================================
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import RepeatedKFold
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import ParameterGrid
+from sklearn.inspection import permutation_importance
+import multiprocessing
+
+# Configuración warnings
+# ==============================================================================
+import warnings
+warnings.filterwarnings('once')
+
+#Cargamos datos 
+PATH = "C:\\Users\marcl\\Desktop\\TFG\\GITHUB TFG\\"
+DATASETS_DIR = PATH + "data\\"
+hoteles = pd.read_pickle(DATASETS_DIR + 'HotelesModelos.pkl')
+hoteles = hoteles.dropna(subset=['precios'], axis=0)
+#juntamos los datos en un DataFrame
+x = pd.DataFrame()
+x['Estrellas'] = hoteles['Estrellas']
+x['precios'] = hoteles['precios']
+x['distancia'] = hoteles['distancia']
+x.info()
+# División de los datos en train y test
+# ==============================================================================
+X_train, X_test, y_train, y_test = train_test_split(
+                                        x.drop(columns= "precios"),
+                                        x['precios'],
+                                        random_state = 123
+                                    )
+# Creación del modelo
+# ==============================================================================
+modelo = RandomForestRegressor(
+            n_estimators = 10,
+            criterion    = 'mse',
+            max_depth    = None,
+            max_features = 'auto',
+            oob_score    = False,
+            n_jobs       = -1,
+            random_state = 123
+         )
+
+# Entrenamiento del modelo
+# ==============================================================================
+modelo.fit(X_train, y_train)
+# Error de test del modelo inicial
+# ==============================================================================
+predicciones = modelo.predict(X = X_test)
+
+rmse = mean_squared_error(
+        y_true  = y_test,
+        y_pred  = predicciones,
+        squared = False
+       )
+print(f"El error (rmse) de test es: {rmse}")
+# Validación empleando el Out-of-Bag error
+# ==============================================================================
+train_scores = []
+oob_scores   = []
+
+# Valores evaluados
+estimator_range = range(1, 150, 5)
+
+# Bucle para entrenar un modelo con cada valor de n_estimators y extraer su error
+# de entrenamiento y de Out-of-Bag.
+for n_estimators in estimator_range:
+    modelo = RandomForestRegressor(
+                n_estimators = n_estimators,
+                criterion    = 'mse',
+                max_depth    = None,
+                max_features = 'auto',
+                oob_score    = True,
+                n_jobs       = -1,
+                random_state = 123
+             )
+    modelo.fit(X_train, y_train)
+    train_scores.append(modelo.score(X_train, y_train))
+    oob_scores.append(modelo.oob_score_)
+    
+# Gráfico con la evolución de los errores
+fig, ax = plt.subplots(figsize=(6, 3.84))
+ax.plot(estimator_range, train_scores, label="train scores")
+ax.plot(estimator_range, oob_scores, label="out-of-bag scores")
+ax.plot(estimator_range[np.argmax(oob_scores)], max(oob_scores),
+        marker='o', color = "red", label="max score")
+ax.set_ylabel("R^2")
+ax.set_xlabel("n_estimators")
+ax.set_title("Evolución del out-of-bag-error vs número árboles")
+plt.legend();
+print(f"Valor óptimo de n_estimators: {estimator_range[np.argmax(oob_scores)]}")
+# Validación empleando k-cross-validation y neg_root_mean_squared_error
+# ==============================================================================
+train_scores = []
+cv_scores    = []
+
+# Valores evaluados
+estimator_range = range(1, 150, 5)
+
+# Bucle para entrenar un modelo con cada valor de n_estimators y extraer su error
+# de entrenamiento y de k-cross-validation.
+for n_estimators in estimator_range:
+    
+    modelo = RandomForestRegressor(
+                n_estimators = n_estimators,
+                criterion    = 'mse',
+                max_depth    = None,
+                max_features = 'auto',
+                oob_score    = False,
+                n_jobs       = -1,
+                random_state = 123
+             )
+    
+    # Error de train
+    modelo.fit(X_train, y_train)
+    predicciones = modelo.predict(X = X_train)
+    rmse = mean_squared_error(
+            y_true  = y_train,
+            y_pred  = predicciones,
+            squared = False
+           )
+    train_scores.append(rmse)
+    
+    # Error de validación cruzada
+    scores = cross_val_score(
+                estimator = modelo,
+                X         = X_train,
+                y         = y_train,
+                scoring   = 'neg_root_mean_squared_error',
+                cv        = 5
+             )
+    # Se agregan los scores de cross_val_score() y se pasa a positivo
+    cv_scores.append(-1*scores.mean())
+    
+# Gráfico con la evolución de los errores
+fig, ax = plt.subplots(figsize=(6, 3.84))
+ax.plot(estimator_range, train_scores, label="train scores")
+ax.plot(estimator_range, cv_scores, label="cv scores")
+ax.plot(estimator_range[np.argmin(cv_scores)], min(cv_scores),
+        marker='o', color = "red", label="min score")
+ax.set_ylabel("root_mean_squared_error")
+ax.set_xlabel("n_estimators")
+ax.set_title("Evolución del cv-error vs número árboles")
+plt.legend();
+print(f"Valor óptimo de n_estimators: {estimator_range[np.argmin(cv_scores)]}")
+# Validación empleando el Out-of-Bag error
+# ==============================================================================
+train_scores = []
+oob_scores   = []
+
+# Valores evaluados
+max_features_range = range(1, X_train.shape[1] + 1, 1)
+
+# Bucle para entrenar un modelo con cada valor de max_features y extraer su error
+# de entrenamiento y de Out-of-Bag.
+for max_features in max_features_range:
+    modelo = RandomForestRegressor(
+                n_estimators = 121,
+                criterion    = 'mse',
+                max_depth    = None,
+                max_features = max_features,
+                oob_score    = True,
+                n_jobs       = -1,
+                random_state = 123
+             )
+    modelo.fit(X_train, y_train)
+    train_scores.append(modelo.score(X_train, y_train))
+    oob_scores.append(modelo.oob_score_)
+    
+# Gráfico con la evolución de los errores
+fig, ax = plt.subplots(figsize=(6, 3.84))
+ax.plot(max_features_range, train_scores, label="train scores")
+ax.plot(max_features_range, oob_scores, label="out-of-bag scores")
+ax.plot(max_features_range[np.argmax(oob_scores)], max(oob_scores),
+        marker='o', color = "red")
+ax.set_ylabel("R^2")
+ax.set_xlabel("max_features")
+ax.set_title("Evolución del out-of-bag-error vs número de predictores")
+plt.legend();
+print(f"Valor óptimo de max_features: {max_features_range[np.argmax(oob_scores)]}")
+# Validación empleando k-cross-validation y neg_root_mean_squared_error
+# ==============================================================================
+train_scores = []
+cv_scores    = []
+
+# Valores evaluados
+max_features_range = range(1, X_train.shape[1] + 1, 1)
+
+# Bucle para entrenar un modelo con cada valor de max_features y extraer su error
+# de entrenamiento y de k-cross-validation.
+for max_features in max_features_range:
+    
+    modelo = RandomForestRegressor(
+                n_estimators = 121,
+                criterion    = 'mse',
+                max_depth    = None,
+                max_features = max_features,
+                oob_score    = True,
+                n_jobs       = -1,
+                random_state = 123
+             )
+    
+    # Error de train
+    modelo.fit(X_train, y_train)
+    predicciones = modelo.predict(X = X_train)
+    rmse = mean_squared_error(
+            y_true  = y_train,
+            y_pred  = predicciones,
+            squared = False
+           )
+    train_scores.append(rmse)
+    
+    # Error de validación cruzada
+    scores = cross_val_score(
+                estimator = modelo,
+                X         = X_train,
+                y         = y_train,
+                scoring   = 'neg_root_mean_squared_error',
+                cv        = 5
+             )
+    # Se agregan los scores de cross_val_score() y se pasa a positivo
+    cv_scores.append(-1*scores.mean())
+    
+# Gráfico con la evolución de los errores
+fig, ax = plt.subplots(figsize=(6, 3.84))
+ax.plot(max_features_range, train_scores, label="train scores")
+ax.plot(max_features_range, cv_scores, label="cv scores")
+ax.plot(max_features_range[np.argmin(cv_scores)], min(cv_scores),
+        marker='o', color = "red", label="min score")
+ax.set_ylabel("root_mean_squared_error")
+ax.set_xlabel("max_features")
+ax.set_title("Evolución del cv-error vs número de predictores")
+plt.legend();
+print(f"Valor óptimo de max_features: {max_features_range[np.argmin(cv_scores)]}")
+# Grid de hiperparámetros evaluados
+# ==============================================================================
+param_grid = ParameterGrid(
+                {'n_estimators': [121],
+                 'max_features': [2],
+                 'max_depth'   : [None, 3, 10, 20]
+                }
+             )
+
+# Loop para ajustar un modelo con cada combinación de hiperparámetros
+# ==============================================================================
+resultados = {'params': [], 'oob_r2': []}
+
+for params in param_grid:
+    
+    modelo = RandomForestRegressor(
+                oob_score    = True,
+                n_jobs       = -1,
+                random_state = 123,
+                ** params
+             )
+    
+    modelo.fit(X_train, y_train)
+    
+    resultados['params'].append(params)
+    resultados['oob_r2'].append(modelo.oob_score_)
+    print(f"Modelo: {params} \u2713")
+
+# Resultados
+# ==============================================================================
+resultados = pd.DataFrame(resultados)
+resultados = pd.concat([resultados, resultados['params'].apply(pd.Series)], axis=1)
+resultados = resultados.drop(columns = 'params')
+resultados = resultados.sort_values('oob_r2', ascending=False)
+resultados.head(4)
+# Mejores hiperparámetros por out-of-bag error
+# ==============================================================================
+print("--------------------------------------------")
+print("Mejores hiperparámetros encontrados (oob-r2)")
+print("--------------------------------------------")
+print(resultados.iloc[0,0], ":", resultados.iloc[0,:]['oob_r2'], "R2")
+
+
+#GridSearch en validación cruzada 
+# Grid de hiperparámetros evaluados
+# ==============================================================================
+param_grid = {'n_estimators': [121],
+              'max_features': [2],
+              'max_depth'   : [None, 3, 10, 20]
+             }
+
+# Búsqueda por grid search con validación cruzada
+# ==============================================================================
+grid = GridSearchCV(
+        estimator  = RandomForestRegressor(random_state = 123),
+        param_grid = param_grid,
+        scoring    = 'neg_root_mean_squared_error',
+        n_jobs     = multiprocessing.cpu_count() - 1,
+        cv         = RepeatedKFold(n_splits=5, n_repeats=3, random_state=123), 
+        refit      = True,
+        verbose    = 0,
+        return_train_score = True
+       )
+
+grid.fit(X = X_train, y = y_train)
+
+# Resultados
+# ==============================================================================
+resultados = pd.DataFrame(grid.cv_results_)
+resultados.filter(regex = '(param.*|mean_t|std_t)') \
+    .drop(columns = 'params') \
+    .sort_values('mean_test_score', ascending = False) \
+    .head(4)
+    
+# Mejores hiperparámetros por validación cruzada
+# ==============================================================================
+print("----------------------------------------")
+print("Mejores hiperparámetros encontrados (cv)")
+print("----------------------------------------")
+print(grid.best_params_, ":", grid.best_score_, grid.scoring)
+#Yo pensaba que era el de max depth = 10
+# Error de test del modelo final
+# ==============================================================================
+modelo_final = grid.best_estimator_
+predicciones = modelo.predict(X = X_test)
+rmse = mean_squared_error(
+        y_true  = y_test,
+        y_pred  = predicciones,
+        squared = False
+       )
+print(f"El error (rmse) de test es: {rmse}")
+
+##Importancia de los predictores por PUREZA DE NODOS 
+importancia_predictores = pd.DataFrame(
+                            {'predictor': x.drop(columns= "precios").columns,
+                             'importancia': modelo.feature_importances_}
+                            )
+print("Importancia de los predictores en el modelo")
+print("-------------------------------------------")
+#Importancia de los predictores por PERMUTACIÓN
+importancia_predictores.sort_values('importancia', ascending=False)
+importancia = permutation_importance(
+                estimator    = modelo_final,
+                X            = X_train,
+                y            = y_train,
+                n_repeats    = 5,
+                scoring      = 'neg_root_mean_squared_error',
+                n_jobs       = multiprocessing.cpu_count() - 1,
+                random_state = 123
+             )
+
+# Se almacenan los resultados (media y desviación) en un dataframe
+df_importancia = pd.DataFrame(
+                    {k: importancia[k] for k in ['importances_mean', 'importances_std']}
+                 )
+df_importancia['feature'] = X_train.columns
+df_importancia.sort_values('importances_mean', ascending=False)
+# Gráfico
+fig, ax = plt.subplots(figsize=(5, 6))
+df_importancia = df_importancia.sort_values('importances_mean', ascending=True)
+ax.barh(
+    df_importancia['feature'],
+    df_importancia['importances_mean'],
+    xerr=df_importancia['importances_std'],
+    align='center',
+    alpha=0
+)
+ax.plot(
+    df_importancia['importances_mean'],
+    df_importancia['feature'],
+    marker="D",
+    linestyle="",
+    alpha=0.8,
+    color="r"
+)
+ax.set_title('Importancia de los predictores (train)')
+ax.set_xlabel('Incremento del error tras la permutación');
